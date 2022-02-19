46fe8a44b74f07c0894c2620c3c4cd1f3134bf8e
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-19 11:16:56 +0530

    Welcome to Gambling Simulator Problem

---
+++ src/GamblingSimulatorProblem/GamblingSimulatorProblem.java
@@ -0,0 +1,10 @@
+package GamblingSimulatorProblem;
+
+public class GamblingSimulatorProblem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulator problem");
+
+	}
+
+}
