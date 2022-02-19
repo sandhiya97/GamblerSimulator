eb782b3216179d1ddc446cf8e55797895b1eb71d
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-19 11:27:38 +0530

    Initializing the bet amount per game and the starting amount

--- src/GamblingSimulatorProblem/GamblingSimulatorProblem.java
+++ src/GamblingSimulatorProblem/GamblingSimulatorProblem.java
@@ -4,7 +4,8 @@ public class GamblingSimulatorProblem {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulator problem");
-
+		int stakeAmount;
+		int betAmountPerGame;
 	}
 
 }
